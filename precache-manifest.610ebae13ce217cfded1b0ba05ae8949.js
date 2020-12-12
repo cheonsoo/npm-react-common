043b2971aa66dbdeb7e7c0d1c3af5041918b10f1
chance <cs.park@gsshop.com>
2020-12-12 12:39:48 +0900

    Updates

---
+++ precache-manifest.610ebae13ce217cfded1b0ba05ae8949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbda8b1c2b3edd608a154bd0c55ff860",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7d0ca5d4dc90633306f9",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "81afc9e1e676a7ce2eca",
+    "url": "./static/js/2.8bc6ba70.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.8bc6ba70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0ca5d4dc90633306f9",
+    "url": "./static/js/main.6940e001.chunk.js"
+  },
+  {
+    "revision": "5ca09c5e96f166a91146",
+    "url": "./static/js/runtime-main.ec201b97.js"
+  }
+]);
